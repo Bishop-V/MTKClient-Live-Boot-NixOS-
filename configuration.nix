a2ae08d4b2fdae34be5b465df5ac772270e02f66
Ash Bishop <ash.m.bishop@protonmail.com>
2026-02-20 00:48:14 +0000

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -8,8 +8,6 @@
   networking.networkmanager.enable = true;
 
   i18n.defaultLocale = "en_US.UTF-8";
-
-    
   
   services = {
     xserver = {
@@ -24,6 +22,21 @@
       user = "mtkclient";
     };
   };
+
+    
+  image.modules.iso = {
+    isoImage.squashfsCompression = "zstd -Xcompression-level 6";
+    # faster
+    #isoImage.squashfsCompression = "zstd -Xcompression-level 3";
+    # fastest
+    #isoImage.squashfsCompression = "lz4";
+  };
+
+  boot.loader = {
+    systemd-boot.enable = lib.mkForce true;
+    grub.enable = lib.mkForce false;
+  };
+
   environment.xfce.excludePackages = with pkgs.xfce; [
     #mousepad
     parole
@@ -37,24 +50,11 @@
    # xfce4-terminal 
   ];
 
-  
-  boot.loader = {
-    grub = {
-      enable = true;
-      efiSupport = true;   
-      device = "nodev";          
-      timeoutStyle = "hidden";      
-    };
-    timeout = lib.mkForce 0;                 
-  };
-
-
   # Configure keymap in X11
   services.xserver.xkb = {
     layout = "us";
     variant = "";
   };
-
 
   users.users."mtkclient" = {
     isNormalUser = true;
@@ -79,6 +79,21 @@
     ]);
 
   programs.adb.enable = true;
+
+  home-manager.users.mtkclient = { ... }: {
+    xdg.mimeApps = {
+      enable = true;
+      defaultApplications = {
+        "text/html" = "librewolf.desktop";
+        "x-scheme-handler/http" = "librewolf.desktop";
+        "x-scheme-handler/https" = "librewolf.desktop";
+        "x-scheme-handler/about" = "librewolf.desktop";
+        "x-scheme-handler/unknown" = "librewolf.desktop";
+      };
+    };
+  home.sessionVariables.BROWSER = "librewolf";
+  };
+
   
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
